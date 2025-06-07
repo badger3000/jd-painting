62440d1df5ab83e06160b03823631d239b595c6d
Kyle Ross <kyle@badger3000.com>
2025-06-07 13:53:05 -0700

    package updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "hybrid",
+  adapter: cloudflare(),
+  vite: {
+    define: {
+      "process.env.PUBLIC_SANITY_PROJECT_ID": JSON.stringify(
+        process.env.PUBLIC_SANITY_PROJECT_ID
+      ),
+      "process.env.PUBLIC_SANITY_DATASET": JSON.stringify(
+        process.env.PUBLIC_SANITY_DATASET
+      ),
+    },
+  },
+  integrations: [tailwind(), react()],
+});
