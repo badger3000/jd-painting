35f738f12f9743c911116bace039a049655180e4
Kyle Ross <kyle@badger3000.com>
2025-06-10 18:00:03 -0700

    hero updates

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,27 +7,22 @@ export const Hero = () => {
       <div className="text-center lg:text-start space-y-6">
         <main className="text-5xl md:text-6xl font-bold">
           <h1 className="inline">
-            <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
-              Shadcn
-            </span>{" "}
-            landing page
-          </h1>{" "}
-          for{" "}
-          <h2 className="inline">
-            <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-              React
-            </span>{" "}
-            developers
-          </h2>
+            <span className="inline bg-gradient-to-r from-[#c9aa3a]  to-[#e4d6a6] text-transparent bg-clip-text">
+              Jon David Decrative Painting
+            </span>
+          </h1>
         </main>
 
         <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-          Build your React landing page effortlessly with the required sections
-          to your project.
+          Renowned painting company in Prescott, AZ. Jon David's expertise in
+          decrative painting transforms spaces, servering client in Prescott and
+          surrounding areas.
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <Button className="w-full md:w-1/3">Get Started</Button>
+          <Button variant="ribbon" size="ribbon">
+            Get Started
+          </Button>
         </div>
       </div>
 
