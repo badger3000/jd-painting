d6d3c5ac958347f2880d0357ec0a04a85042eff3
Kyle Ross <kyle@badger3000.com>
2025-07-06 08:55:42 -0700

    Added testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import {Testimonials} from "@/components/Testimonials";
 import {Pricing} from "@/components/Pricing";
 import {FAQ} from "@/components/FAQ";
 export default function Home() {
   return (
     <>
+      <Testimonials />
       <Pricing />
       <FAQ />
     </>
